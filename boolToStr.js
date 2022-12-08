69b1f2cce1b75bb0e757ec12cd30c283ed9eaa9a
Shauna MacFarlane-Okongo <102887963+DrSLMac@users.noreply.github.com>
2022-12-08 12:25:14 -0600

    add booleanToString

---
+++ boolToStr.js
@@ -0,0 +1,13 @@
+// Implement a function which convert the given boolean value into its string representation.
+
+// Note: Only valid inputs will be given.
+
+function booleanToString(b){
+    return b.toString()
+  }
+  
+  // const booleanToString = b => b ? "true" : false
+  // const booleanToString = b => b.toString()
+  
+  console.log("booleanToString(true): ", booleanToString(true))
+  console.log("booleanToString(false): ", booleanToString(false))
